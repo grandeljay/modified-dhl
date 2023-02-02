a51b61dcfa20fcae32a481539946d6cb49418c5a
grandeljay <github.jay@grandel.anonaddy.me>
2023-02-02 14:19:43 +0100

    Improve wording

--- src/lang/english/modules/shipping/grandeljaydhl.php
+++ src/lang/english/modules/shipping/grandeljaydhl.php
@@ -46,7 +46,7 @@ $translations_general = array(
      */
     'SHIPPING_MAX_WEIGHT_TITLE'                                => 'Maximum weight',
     'SHIPPING_MAX_WEIGHT_DESC'                                 => sprintf(
-        'Maximum weight an item may have. Overrides the %s option.',
+        'Maximum weight that a parcel may have. Overrides the %s option.',
         sprintf(
             '<a href="/' . DIR_ADMIN . 'configuration.php?gID=7">%s</a>',
             defined('SHIPPING_MAX_WEIGHT_TITLE') ? SHIPPING_MAX_WEIGHT_TITLE : 'SHIPPING_MAX_WEIGHT',
