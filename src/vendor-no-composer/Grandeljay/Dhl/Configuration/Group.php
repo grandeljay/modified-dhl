bff17df042d5b7313d789824211096ca87c85f08
grandeljay <github.jay@grandel.anonaddy.me>
2023-02-07 18:07:51 +0100

    refactor: fix filenames to match namespace

---
+++ src/vendor-no-composer/Grandeljay/Dhl/Configuration/Group.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Grandeljay\Dhl\Configuration;
+
+use RobinTheHood\ModifiedStdModule\Classes\CaseConverter;
+
+class Group
+{
+    public static function start(string $value, string $option): string
+    {
+        $key_without_module_name = substr($option, strlen(\grandeljaydhl::NAME) + 1);
+        $key_lisp                = CaseConverter::screamingToLisp($key_without_module_name);
+
+        ob_start();
+        ?>
+        <details class="<?= $key_lisp ?>">
+            <summary><?= $value ?></summary>
+            <div>
+        <?php
+        return ob_get_clean();
+    }
+
+    public static function end(string $value, string $option): string
+    {
+        ob_start();
+        ?>
+            </div>
+        </details>
+        <?php
+        return ob_get_clean();
+    }
+
+    /**
+     * Weight
+     */
+    public static function weightStart(string $value, string $option): string
+    {
+        return self::start('<h2>' . $value . '</h2>', $option);
+    }
+
+    public static function weightEnd(string $value, string $option): string
+    {
+        return self::end($value, $option);
+    }
+
+    /**
+     * National
+     */
+    public static function nationalStart(string $value, string $option): string
+    {
+        return self::start('<h2>' . $value . '</h2>', $option);
+    }
+
+    public static function nationalEnd(string $value, string $option): string
+    {
+        return self::end($value, $option);
+    }
+
+    /**
+     * International
+     */
+    public static function internationalStart(string $value, string $option): string
+    {
+        return self::start('<h2>' . $value . '</h2>', $option);
+    }
+
+    public static function internationalEnd(string $value, string $option): string
+    {
+        return self::end($value, $option);
+    }
+
+    /** Premium */
+    public static function internationalPremiumStart(string $value, string $option): string
+    {
+        return self::start('<h3>' . $value . '</h3>', $option);
+    }
+
+    public static function internationalPremiumEnd(string $value, string $option): string
+    {
+        return self::end($value, $option);
+    }
+
+    /** Economy */
+    public static function internationalEconomyStart(string $value, string $option): string
+    {
+        return self::start('<h3>' . $value . '</h3>', $option);
+    }
+
+    public static function internationalEconomyEnd(string $value, string $option): string
+    {
+        return self::end($value, $option);
+    }
+    /** */
+
+    /**
+     * Surcharges
+     */
+    public static function surchargesStart(string $value, string $option): string
+    {
+        return self::start('<h2>' . $value . '</h2>', $option);
+    }
+
+    public static function surchargesEnd(string $value, string $option): string
+    {
+        return self::end($value, $option);
+    }
+}
---
+++ src/vendor-no-composer/Grandeljay/Dhl/Parcel.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Grandeljay\Dhl;
+
+class Parcel
+{
+    private static array $sizes = array(
+        array(
+            'weight' => 2,
+
+            'length' => 60,
+            'width'  => 30,
+            'height' => 15,
+        ),
+        array(
+            'weight' => 5,
+
+            'length' => 120,
+            'width'  => 60,
+            'height' => 60,
+        ),
+        array(
+            'weight' => 10,
+
+            'length' => 120,
+            'width'  => 60,
+            'height' => 60,
+        ),
+        array(
+            'weight' => 31.5,
+
+            'length' => 120,
+            'width'  => 60,
+            'height' => 60,
+        ),
+    );
+
+    private float $weight   = 0;
+    private array $prodcuts = array();
+
+    public function __construct()
+    {
+    }
+
+    /**
+     * Weight
+     */
+    public function getWeight(): float
+    {
+        return $this->weight;
+    }
+
+    public function setWeight(float $weight): void
+    {
+        $this->weight = $weight;
+    }
+    /** */
+
+    /**
+     * Dimensions
+     */
+    private function getDimension(string $dimension): float
+    {
+        foreach (self::$sizes as $parcel) {
+            if ($this->weight <= $parcel['weight']) {
+                return $parcel[$dimension];
+            }
+        }
+    }
+
+    public function getLength(): float
+    {
+        return getDimension('length');
+    }
+
+    public function getWidth(): float
+    {
+        return getDimension('width');
+    }
+
+    public function getHeight(): float
+    {
+        return getDimension('height');
+    }
+    /** */
+
+    /**
+     * Products
+     */
+    public function addProduct(): void
+    {
+        $this->products[] = $prodcut['id'];
+        $this->weight    += $product['weight'];
+    }
+    /** */
+}
