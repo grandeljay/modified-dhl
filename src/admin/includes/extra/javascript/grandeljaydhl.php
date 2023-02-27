c2e8d820416dab2753010a78b76e915ee25ec605
grandeljay <github.jay@grandel.anonaddy.me>
2023-02-27 12:27:52 +0100

    fix: auto include js on admin screen

--- src/admin/includes/extra/javascript/grandeljaydhl.php
+++ src/admin/includes/extra/javascript/grandeljaydhl.php
@@ -8,6 +8,23 @@
  * @package GrandelJayDHL
  */
 
+if (rth_is_module_disabled('MODULE_SHIPPING_GRANDELJAYDHL')) {
+    return;
+}
+
+/** Only enqueue JavaScript when module settings are open */
+$grandeljayups_admin_screen = array(
+    'set'    => 'shipping',
+    'module' => grandeljaydhl::class,
+    'action' => 'edit',
+);
+
+parse_str($_SERVER['QUERY_STRING'] ?? '', $query_string);
+
+if ($query_string !== $grandeljaydhl_admin_screen) {
+    return;
+}
+
 $file_name    = '/' . DIR_ADMIN . 'includes/javascript/grandeljaydhl.js';
 $file_path    = DIR_FS_CATALOG .  $file_name;
 $file_version = hash_file('crc32c', $file_path);
