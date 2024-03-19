e6785f74d93b5966c32bbe93fc78c7a1495e5d32
grandeljay <github@grandels.email>
2024-03-19 08:16:31 +0100

    chore(module): update to v0.7.0

--- src/includes/modules/shipping/grandeljaydhl.php
+++ src/includes/modules/shipping/grandeljaydhl.php
@@ -22,7 +22,7 @@ use RobinTheHood\ModifiedStdModule\Classes\StdModule;
 class grandeljaydhl extends StdModule
 {
     public const NAME    = 'MODULE_SHIPPING_GRANDELJAYDHL';
-    public const VERSION = '0.6.8';
+    public const VERSION = '0.7.0';
 
     public static function setFunction(string $value, string $option): string
     {
